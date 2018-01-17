b5f440f37639a6c4586738270e366435eecffdb1
EC2 Default User <ec2-user@ip-172-31-39-41.us-east-2.compute.internal>
2018-01-17 18:46:00 +0000

    database added, show page for single campground added

--- app.js
+++ app.js
@@ -1,46 +1,81 @@
-var express=require("express");
-var app=express();
-var bodyParser=require("body-parser");
+var express   =require("express"),
+    app       =express(),
+    bodyParser=require("body-parser"),
+    mongoose  =require("mongoose");
 
+mongoose.connect("mongodb://localhost/yelp_camp");
 app.use(bodyParser.urlencoded({extended:true}));
-
 app.set("view engine","ejs");
+
+
+var campgroundSchema= new mongoose.Schema({
+    name:String,
+    image:String,
+    description:String
+});
+
+var Campground=mongoose.model("Campground",campgroundSchema);
+
+// Campground.create({
+//   name:"Black Lake",
+//   image:"https://static1.squarespace.com/static/57a33100579fb3f47b0e4f5f/57c33c569f745643297c9a2b/57c33c568419c2d24d5e3839/1472412848948/Campsite7.jpg",
+//   description:"This is a huge campground with lake viwe and no toilets"
+// },function(err,campground){
+//     if(err){
+//         console.log(err)
+//     }else{
+//         console.log("Newly created campground:");
+//         console.log(campground);
+//     }
+// });
 
 app.get("/",function(req,res){
     res.render("landing");
 });
 
-var campgrounds=[
-            {name:"Salmon creek",image:"https://grist.files.wordpress.com/2017/05/tent-campsite-by-river.jpg?w=1024&h=576&crop=1"},
-            {name:"Black Lake",image:"https://static1.squarespace.com/static/57a33100579fb3f47b0e4f5f/57c33c569f745643297c9a2b/57c33c568419c2d24d5e3839/1472412848948/Campsite7.jpg"},
-            {name:"Tahoe river",image:"https://media-cdn.tripadvisor.com/media/photo-s/05/c6/94/80/red-squirrel-campsite.jpg"},
-            {name:"Serious river",image:"https://www.oars.com/wp-content/uploads/2014/07/Rogue-JamesKaiser-570.web_-1024x684.jpg"},
-            {name:"Salmon creek",image:"https://grist.files.wordpress.com/2017/05/tent-campsite-by-river.jpg?w=1024&h=576&crop=1"},
-            {name:"Black Lake",image:"https://static1.squarespace.com/static/57a33100579fb3f47b0e4f5f/57c33c569f745643297c9a2b/57c33c568419c2d24d5e3839/1472412848948/Campsite7.jpg"},
-            {name:"Tahoe river",image:"https://media-cdn.tripadvisor.com/media/photo-s/05/c6/94/80/red-squirrel-campsite.jpg"},
-            {name:"Serious river",image:"https://www.oars.com/wp-content/uploads/2014/07/Rogue-JamesKaiser-570.web_-1024x684.jpg"},
-            {name:"Salmon creek",image:"https://grist.files.wordpress.com/2017/05/tent-campsite-by-river.jpg?w=1024&h=576&crop=1"},
-            {name:"Black Lake",image:"https://static1.squarespace.com/static/57a33100579fb3f47b0e4f5f/57c33c569f745643297c9a2b/57c33c568419c2d24d5e3839/1472412848948/Campsite7.jpg"},
-            {name:"Tahoe river",image:"https://media-cdn.tripadvisor.com/media/photo-s/05/c6/94/80/red-squirrel-campsite.jpg"},
-            {name:"Serious river",image:"https://www.oars.com/wp-content/uploads/2014/07/Rogue-JamesKaiser-570.web_-1024x684.jpg"}
-        ];
+
 
 app.get("/campgrounds",function(req,res){
     
-        
-    res.render("campgrounds",{campgrounds:campgrounds});
+        Campground.find({},function(err,allCampgrounds){
+            if(err){
+                console.log(err);
+            }else{
+                res.render("index",{campgrounds:allCampgrounds});
+            }
+        })
+    
 });
 
 app.post("/campgrounds",function(req,res){
     var name=req.body.name;
     var image=req.body.image;
-    var newCampground={name: name,image: image};
-    campgrounds.push(newCampground);
-    res.redirect("/campgrounds");
+    var desc=req.body.description;
+    var newCampground={name: name,image: image,description: desc};
+    
+    Campground.create(newCampground,function(err,newlyCreated){
+        if(err){
+            console.log(err);
+        }else{
+            res.redirect("/campgrounds");
+        }
+    });
 });
 
 app.get("/campgrounds/new",function(req, res) {
     res.render("new")
+})
+
+app.get("/campgrounds/:id",function(req, res) {
+    Campground.findById(req.params.id,function(err,foundCampground){
+       if(err){
+           console.log(err)
+       } else{
+           res.render("show",{campground:foundCampground});
+       }
+    });
+    
+    
 })
 
 app.listen(process.env.PORT,process.env.IP,function(){

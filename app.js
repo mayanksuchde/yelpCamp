41e563cdfb524f3bac3c025614bfa60403cbcb08
mayanksuchde <mayank.suchde.4500@gmail.com>
2018-01-26 17:05:27 +0000

    add mlab

--- app.js
+++ app.js
@@ -17,7 +17,10 @@ var commentRoutes=require("./routes/comments"),
     indexRoutes=require("./routes/index");
     
 
-mongoose.connect("mongodb://localhost/yelp_camp");
+//mongoose.connect("mongodb://localhost/yelp_camp");
+mongoose.connect("mongodb://mayank:mayank@ds117148.mlab.com:17148/yelpcamp6692");
+
+mongodb://mayank:mayank@ds117148.mlab.com:17148/yelpcamp6692
 app.use(bodyParser.urlencoded({extended:true}));
 app.set("view engine","ejs");
 app.use(express.static(__dirname + "/public"));
